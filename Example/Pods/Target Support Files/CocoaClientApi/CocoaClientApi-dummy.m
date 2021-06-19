b7166cafd8b613f9928468d5e5f4e5653e3507ab
Andres Azofeifa Cascante <aazofeifa1989@hotmail.com>
2021-06-18 23:20:26 -0600

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/CocoaClientApi/CocoaClientApi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CocoaClientApi : NSObject
+@end
+@implementation PodsDummy_CocoaClientApi
+@end
---
+++ Example/Pods/Target Support Files/Pods-CocoaClientApi_Example/Pods-CocoaClientApi_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoaClientApi_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CocoaClientApi_Example
+@end
